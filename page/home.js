2ef38155172882bc567af7af1e85351006448d8f
jp <617298698@qq.com>
2017-08-27 23:19:05 +0800

    home

--- page/home.js
+++ page/home.js
@@ -6,24 +6,36 @@ var template = `<div class='home'>
 	<option  value = "hangzhou" >hangzhou</option >
 </select >
 <section>
-<image src='img/quest1.jpg' alt='quest1'></image>
+	<div class='qCard'>
+		<img src='img/quest1.jpg'></img>
+		<div>title: girl1</div>
+	</div>
+	<div class='qCard'>
+		<img src='img/q2.jpg'></img>
+		<div>title: girl2</div>
+	</div>
 </section>
+<nav>
+	<a href="index.asp">Home</a>
+	<a href="html5_meter.asp">Previous</a>
+	<a href="html5_noscript.asp">Next</a>
+</nav>
 </div>`
 
 const Home = {
 	template: template,
 	data: function(){
 		return {
-            searchTitle:'',
+      searchTitle:'',
 			palce:''
 		}
 	},
-    methods:{
-        getAuthCode: function(){
-            this.authCode = '2333'
-        },
-        login: function(){
-            router.push('/')
-        }
+  methods:{
+    getAuthCode: function(){
+        this.authCode = '2333'
+    },
+    login: function(){
+        router.push('/')
     }
+  }
 }
